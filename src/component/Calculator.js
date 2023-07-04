c4f6bd3b2e3eb8b5846cf1a856947e2317f37c79
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-07-04 16:39:42 +0300

    Add Events

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputButton from './Numbers';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
-const number = ['AC', '\u00B1', '%', '\u00F7', 7, 8, 9, '\u00D7', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+const number = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
 
 function Calculator() {
+  const [result, setResult] = useState({ total: null, next: null, operation: null });
+  const { total, next, operation } = result;
+
+  const handleInputBtn = (e) => {
+    const input = e.target.innerHTML;
+    setResult(calculate(result, input));
+  };
+
   return (
     <div className="calc-container">
-      <div className="resultScreen">0</div>
-      {number.map((n) => (n === '\u00F7' || n === '\u00D7' || n === '-' || n === '+' || n === '=' ? <InputButton inputSymbol={n} key={n} background="red" /> : <InputButton inputSymbol={n} key={n} background="gray" />))}
+      <div className="resultScreen">{next || operation || total || 0}</div>
+      {number.map((n) => (
+        n === '÷' || n === 'x' || n === '-' || n === '+' || n === '=' ? (
+          <InputButton inputSymbol={n} key={n} background="red" handleInputBtn={handleInputBtn} />
+        ) : (
+          <InputButton inputSymbol={n} key={n} background="gray" handleInputBtn={handleInputBtn} />
+        )
+      ))}
     </div>
   );
 }
